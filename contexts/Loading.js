c8eb5ca3c720554bdc82051bdffe7be4a56842ec
mastyDev <mastydev@gmail.com>
2022-06-20 20:14:53 +0200

    Upgrade Head>title in Loading.js

--- contexts/Loading.js
+++ contexts/Loading.js
@@ -4,7 +4,7 @@ import { Circle } from 'better-react-spinkit'
 export default function Loading() {
   return (
     <>
-    <Head><title>TEN | Loading...</title></Head>
+    <Head><title>TEN | CMS</title></Head>
     <Circle size={32} color='var(--darkest-blue)' style={{
             height:'100vh', 
             display: 'flex',
